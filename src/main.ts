4bc33f5c5ba93b91dac179b1e99f681928576929
moazam-attiq-01 <moazam498.mam@gmail.com>
2025-11-18 16:14:34 +0500

    old main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import session from 'express-session'
+import { ConfigService } from '@nestjs/config'
+import { PostgresExceptionFilter } from './utils/postgres-exception.filter'
+import { AllExceptionsFilter } from './utils/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule)
+  const config = app.get(ConfigService)
   app.enableCors({
     origin: true,
     credentials: true,
-  });
-
-  await app.listen(process.env.PORT || 3000);
-  console.log(`App running locally on http://localhost:3000`);
+  })
+  app.use(
+    session({
+      secret: config.getOrThrow<string>('SESSION_SECRET'),
+      resave: true,
+      rolling: true,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24,
+        secure: false,
+        sameSite: 'lax',
+      },
+    }),
+  )
+  app.useGlobalFilters(new PostgresExceptionFilter())
+  app.useGlobalFilters(new AllExceptionsFilter())
+  await app.listen(process.env.PORT ?? 3000)
 }
-
-bootstrap();
+bootstrap()
