ce0b2de867ae3b497a594e433cac3ab127cf7fd3
moazam-attiq-01 <moazam498.mam@gmail.com>
2025-11-19 19:11:18 +0500

    changed a few settings

--- src/vendor/auth/auth.guard.ts
+++ src/vendor/auth/auth.guard.ts
@@ -1,17 +1,20 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
-import { Request } from 'express'
-import { Observable } from 'rxjs'
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import type { Request } from "express";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    const req = context.switchToHttp().getRequest<Request>()
-    console.log("Session: ", req.session);
-    
-    if (req.session.vendor) {
-      return true
-    } else {
-      throw new UnauthorizedException('Unauthorized')
-    }
+    const req = context.switchToHttp().getRequest();
+    // Debugging: log session
+    console.log("Session:", req.session);
+
+    if (req.session.vendor) return true;
+    throw new UnauthorizedException("Unauthorized");
   }
 }
