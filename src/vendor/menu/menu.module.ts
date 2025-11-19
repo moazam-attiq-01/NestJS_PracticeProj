fa3adbd7556e844337232a7b7245feed35724066
moazam-attiq-01 <moazam498.mam@gmail.com>
2025-11-19 13:39:11 +0500

    imported cloud service in menu module

--- src/vendor/menu/menu.module.ts
+++ src/vendor/menu/menu.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common'
-import { MenuService } from './menu.service'
-import { MenuController } from './menu.controller'
+// src/menu/menu.module.ts
+import { Module } from '@nestjs/common';
+import { MenuService } from './menu.service';
+import { MenuController } from './menu.controller';
+import { DbModule } from '../../db/db.module'; // your DB provider module
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
-  controllers: [MenuController],
+  imports: [DbModule, CloudinaryModule], // <- import here
   providers: [MenuService],
+  controllers: [MenuController],
 })
 export class MenuModule {}
