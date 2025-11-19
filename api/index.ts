50edf484437eab4a7c54dcf63ac63a12f621c825
moazam-attiq-01 <moazam498.mam@gmail.com>
2025-11-19 18:12:15 +0500

    changed file to api/index.ts from serverless.ts

---
+++ api/index.ts
@@ -0,0 +1,55 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../dist/src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import session from 'express-session';
+import { ConfigService } from '@nestjs/config';
+import { PostgresExceptionFilter } from '../dist/src/utils/postgres-exception.filter';
+import { AllExceptionsFilter } from '../dist/src/utils/http-exception.filter';
+
+const expressApp = express();
+let cachedApp;
+
+async function createNestServer() {
+  if (!cachedApp) {
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+
+    const config = app.get(ConfigService);
+
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+
+    app.use(
+      session({
+        secret: config.getOrThrow<string>('SESSION_SECRET'),
+        resave: true,
+        rolling: true,
+        saveUninitialized: false,
+        cookie: {
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24,
+          secure: true,
+          sameSite: 'lax'
+        },
+      }),
+    );
+
+    app.useGlobalFilters(new PostgresExceptionFilter());
+    app.useGlobalFilters(new AllExceptionsFilter());
+
+    await app.init();
+    cachedApp = app;
+  }
+
+  return cachedApp;
+}
+
+export default async function handler(req, res) {
+  await createNestServer();
+  return expressApp(req, res);
+}
